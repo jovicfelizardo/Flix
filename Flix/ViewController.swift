cdaffeb7fe957a863a9815889c376bfd80ef41fb
Bryant Tran <bryant.n.tran@sjsu.edu>
2020-02-20 21:35:31 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Bryant Tran on 2/20/20.
+//  Copyright © 2020 bryanttran1999. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
